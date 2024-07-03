d6886740d076a9307d965037f6573405d6d656df
TeslA1402 <ivantimeev95@gmail.com>
2024-07-03 18:36:50 +0500

    Init commit

---
+++ src/test/java/org/example/skillboxtasktracker/SkillboxTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skillboxtasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillboxTaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
